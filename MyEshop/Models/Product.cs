fd8941b68e8462d26fe788a5823aba97c818e6ca
iliya <iliyahm@yahoo.com>
2025-02-21 21:51:45 +0000

    used code first and made db

--- MyEshop/Models/Product.cs
+++ MyEshop/Models/Product.cs
@@ -2,14 +2,18 @@
 {
     public class Product
     {
-        public Product() { 
-        Categories = new List<Category>();
-        }
+        //public Product() { 
+        //Categories = new List<Category>();
+        //}
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int ItemId{ get; set; }
 
-        public List <Category> Categories { get; set; }
+        //public List <Category> Categories { get; set; }
 
+        public ICollection<CategoryToProduct> categoryToProducts { get; set; }
+
+        public Item Item{ get; set; }
     }
 }
